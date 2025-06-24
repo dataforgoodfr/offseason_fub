f8829ac68a2c35595403ab32b7fb79a399640b0d
thibaut1998e <thibaut.eloy1998@gmail.com>
2025-06-24 22:46:15 +0200

    nettoyage_donnees_v2

--- code/local_paths.py
+++ code/local_paths.py
@@ -1,4 +1,4 @@
-
+"""fichier contenant des chemins sur nos machines en local. Ce fichier a été ajouté au gitignore"""
 
 your_local_save_fold = "/home/thibaut/Documents/fub_results" # chemin local pour la sauvegarde des histogrammes, tableaux de notes, pour une visualisation plus rapide
                                                                 # que lorsque les données sont sauvegadéees sur le s3
